d4b7affd080e6b8a28caa6a4b6e6a4cb8cc0c47a
cr1337-sudo <cristiancuello10@gmail.com>
2023-12-13 19:55:59 -0300

    Done servicio upload video service

--- src/services/upload-video.service.ts
+++ src/services/upload-video.service.ts
@@ -1,7 +1,35 @@
 import { generateUniqueId } from '../utils/generateUniqueId';
 import { validateValidVideoFormat } from '../validators';
-import { extractVideoType } from '../utils/extractVideoType';
 import { AwsS3Repo } from '../repositories/aws-s3.repo';
+import { PassThrough, Readable } from 'stream';
+import { path } from '@ffmpeg-installer/ffmpeg'
+import ffmpeg from 'fluent-ffmpeg'
+import fs from 'fs/promises'
+
+
+async function convertVideoToMp3(bufferVideo: any, videoName: string) {
+    return new Promise((resolve, reject) => {
+        const mp3Path = './videos/' + videoName;
+        const readableBufferVideo = Readable.from(bufferVideo)
+        const command = ffmpeg();
+        command.setFfmpegPath(path)
+        command.input(readableBufferVideo);
+        command.format('mp3')
+        command.output(mp3Path)
+        command.on('end', () => {
+            return resolve(mp3Path);
+        });
+        command.on('start', (commandLine) => {
+            console.log('ffmpeg started conversion', commandLine)
+        })
+        command.on('stderr', function (stderrLine) {
+            console.error('Stderr output: ' + stderrLine)
+        })
+        command.on('error', (err: any) => reject(err));
+
+        command.run();
+    });
+}
 
 export class UploadVideoService {
     private static instance: UploadVideoService;
@@ -24,23 +52,21 @@ export class UploadVideoService {
                 return { success: false, message: "Unable to Upload the file", data: 'Invalid video format' };
             }
 
-            const videoName = generateUniqueId() + '.' + extractVideoType(video.mimetype)
+            const videoName = generateUniqueId() + '.mp3'
+            const mp3Path = await convertVideoToMp3(video.buffer, videoName) as string;
+            const mp3File = await fs.readFile(mp3Path)
             const params = {
-                Bucket: "videos-dev-cristian",
+                Bucket: "mp3-dev-cristian",
                 Key: videoName,
-                Body: video.buffer
+                Body: mp3File
             };
 
-            try {
-                await this.awsS3Repo.uploadFile(params)
+            await this.awsS3Repo.uploadFile(params)
+            await fs.rm(mp3Path)
 
-                return { success: true, message: "File Uploaded with Successfull", videoName };
-            } catch (error) {
-                return { success: false, message: "Unable to Upload the file", data: error };
-            }
-
+            return { success: true, message: "File Uploaded with Successfull", videoName };
         } catch (error) {
-            return { success: false, message: "Unalbe to access this file", data: {} };
+            return { success: false, message: "Unable to Upload the file", data: error };
         }
     }
 
