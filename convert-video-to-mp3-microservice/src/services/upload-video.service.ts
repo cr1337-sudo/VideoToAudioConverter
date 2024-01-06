57dae0c525e7052932acf25eb0def1e278e31ba3
cr1337-sudo <cristiancuello10@gmail.com>
2024-01-06 17:05:31 -0300

    Cambios servicio upload video

--- convert-video-to-mp3-microservice/src/services/upload-video.service.ts
+++ convert-video-to-mp3-microservice/src/services/upload-video.service.ts
@@ -9,7 +9,7 @@ import fs from 'fs/promises'
 
 async function convertVideoToMp3(bufferVideo: any, videoName: string) {
     return new Promise((resolve, reject) => {
-        const mp3Path = './videos/' + videoName;
+        const mp3Path = './mp3/' + videoName;
         const readableBufferVideo = Readable.from(bufferVideo)
         const command = ffmpeg();
         command.setFfmpegPath(path)
@@ -25,7 +25,9 @@ async function convertVideoToMp3(bufferVideo: any, videoName: string) {
         command.on('stderr', function (stderrLine) {
             console.error('Stderr output: ' + stderrLine)
         })
-        command.on('error', (err: any) => reject(err));
+        command.on('error', (err: any) => {
+            return reject(err)
+        });
 
         command.run();
     });
